8855edcd63b2d04adaf64134cc3cfe0459f147b0
Olena Abramova <olena.v.abramova@gmail.com>
2021-03-16 16:56:57 +0200

    create fetch function

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,2 +1,8 @@
 const API_BASE = 'https://pixabay.com/api/?key=';
 const API_KEY = '20710280-32e8ecf65799bd35d240b13ea';
+
+function fetchImages(img) {
+  return fetch(`${API_BASE}${API_KEY}`).then(res => res.json());
+}
+
+export default fetchImages;
