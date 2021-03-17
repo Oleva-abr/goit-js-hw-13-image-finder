ce92aff261ba5ac9ac0570fc428df9ec4ea8f73a
Olena Abramova <olena.v.abramova@gmail.com>
2021-03-17 09:14:34 +0200

    change function on async

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,10 +1,10 @@
 const API_BASE = 'https://pixabay.com/api/?key=';
 const API_KEY = '20710280-32e8ecf65799bd35d240b13ea';
 
-function fetchImages(img) {
-  return fetch(`${API_BASE}${API_KEY}`)
-    .then(res => res.json())
-    .then(console.log);
+async function fetchImages(img) {
+const response = fetch(`${API_BASE}${API_KEY}`);
+const images = response.json();
+  return images;
 }
 
 export default fetchImages;
