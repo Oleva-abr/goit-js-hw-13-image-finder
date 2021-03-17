d7c73872843ae3449a9cdee341ed81ef53a5cc49
Olena Abramova <olena.v.abramova@gmail.com>
2021-03-17 16:53:21 +0200

    add some changes

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,10 +1,12 @@
-const API_BASE = 'https://pixabay.com/api/?key=';
+const API_URL = 'https://pixabay.com/api';
 const API_KEY = '20710280-32e8ecf65799bd35d240b13ea';
 
-async function fetchImages(img) {
-const response = fetch(`${API_BASE}${API_KEY}`);
-const images = response.json();
-  return images;
+async function fetchImages(imgId) {
+  return fetch(`${API_URL}/?key=${API_KEY}/${imgId}`).then(res => {
+    if (!res.ok) {
+      return Promise.reject('No image found');
+    }
+    return res.json();
+  });
 }
-
 export default fetchImages;
--- src/js/imagefinder.js
+++ src/js/imagefinder.js
@@ -1,1 +1,15 @@
 import fetchImages from './apiService';
+
+const form = document.querySelector('.input');
+const gallery = document.querySelector('.gallery');
+
+form.addEventListener('input', getImgByID);
+
+function getImgByID(event) {
+  clearValue();
+
+  const countryName = event.target.value;
+  if (!countryName) {
+    return;
+  }
+}
